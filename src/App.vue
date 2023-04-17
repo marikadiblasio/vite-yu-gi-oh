b3d2094e285387c2f3ca5fccb99a40fc0035373c
marikadiblasio <marbla.albram@gmail.com>
2023-04-17 15:11:22 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

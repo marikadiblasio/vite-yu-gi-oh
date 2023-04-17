b7fdf497435755a034c8b9acbee0023d7780dd61
marikadiblasio <marbla.albram@gmail.com>
2023-04-17 18:00:33 +0200

    add header - fixed styles

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,13 +1,13 @@
 <template>
     <main>
-        <div class="container"> 
-            <select name="archs" id="archs" v-model="selectedType">
+        <div class="container p-3"> 
+            <select class="py-1 rounded" name="archs" id="archs" v-model="selectedType">
                 <option v-for="(type, i) in store.Archetypes" :key="i" :value="type.archetype_name">{{ type.archetype_name }}</option>
             </select>
         </div>
         <div class="container bg-white">
             <div class="row ">
-                <div class="col-12 bg-dark text-white">Found {{store.CharsList.length}} cards</div>
+                <div class="col-12 py-2 bg-dark text-white">Found {{store.CharsList.length}} cards</div>
                <CharacterCard :class="{'d-none': selectedType !== char.archetype}" :image="char.card_images[0].image_url" :name="char.name" :archetype="char.archetype" v-for="(char, index) in store.CharsList" :key="char.id"/>
             </div>
         </div>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,7 @@
 <template>
-    <header class="bg-white">
-        Header
+    <header class="bg-white d-flex ps-1 align-items-center">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg" alt="logo">
+        <h1 class="ps-1">Yu-Gi-Oh-Api</h1>
     </header>
 </template>
 
@@ -11,5 +12,8 @@
 </script>
 
 <style lang="scss" scoped>
-
+    img{
+        width: 6rem;
+        height: 3rem;
+    }
 </style>
